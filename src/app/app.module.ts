66f44671e1b7f0a8608dbf66620c344baa28e008
chaloemphong <chaloemphong.sa@gmail.com>
2022-03-19 00:07:46 +0700

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './modules/user/user.component';
+import { TopComponent } from './layout/top/top.component';
+import { BottomComponent } from './layout/bottom/bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TopComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
