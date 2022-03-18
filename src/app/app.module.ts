aed4ba9119d9c537a23bca1de05feaba4d12622d
chaloemphong <chaloemphong.sa@gmail.com>
2022-03-18 22:23:32 +0700

    ลบ component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 import { UserComponent } from './modules/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
     UserComponent
   ],
   imports: [
